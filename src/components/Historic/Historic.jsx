bcbc5d4461e327fa215f8bc6cc7e18627f68b64d
Marcel George Ionite <george_ionite@yahoo.com>
2024-02-07 21:31:49 +0000

    Historic charts basic version added.

---
+++ src/components/Historic/Historic.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+
+function Historic() {
+  const [currencyData, setCurrencyData] = useState(null);
+  useEffect(() => {
+    const apiKey = "NPflPcQAwrwffLs9iVzHdDWyrqSSRNxm";
+    const url = `https://financialmodelingprep.com/api/v3/historical-price-full/EURUSD?apikey=${apiKey}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCurrencyData(data); // Save the API data to state
+        console.log(currencyData);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []); // The empty dependency array ensures this effect runs once after the initial render
+
+  return (
+    <>
+      <div>
+        <h2>Multiple Graphs</h2>
+        <ChartComponent id="container" primaryXAxis={{ valueType: "Category" }}>
+          <Inject services={[Legend, Tooltip]} />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={currencyData}
+              xName="date"
+              yName="open"
+              name="Open"
+              type="Line"
+            />
+            <SeriesDirective
+              dataSource={currencyData}
+              xName="date"
+              yName="high"
+              name="High"
+              type="Line"
+            />
+            <SeriesDirective
+              dataSource={currencyData}
+              xName="date"
+              yName="low"
+              name="Low"
+              type="Line"
+            />
+            <SeriesDirective
+              dataSource={currencyData}
+              xName="date"
+              yName="close"
+              name="Close"
+              type="Line"
+            />
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </>
+  );
+}
+
+export default Historic;

86e7966a063418454489ecf09aa574d5d7a0c4f8
Noah Sheldon <noahsheldon06@gmail.com>
2024-02-09 18:28:42 +0000

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import "./App.css";
-// import Header from "./components/Header/Header";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NoPage from "./components/NoPage/NoPage";

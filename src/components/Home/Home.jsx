043e08b098140afba5e723ea8a7c8fc754178d10
Noah Sheldon <noahsheldon06@gmail.com>
2024-02-09 20:22:37 +0000

    home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,64 @@
-export default function Home() {
+import React from "react";
+import {
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Home = () => {
   return (
-    <>
-      <div className="container">
-        <h1>Home</h1>
-        <h1>Home</h1>
-        <h1>Home</h1>
-        <h1>Home</h1>
-        <h1>Home</h1>
-        <h1>Home</h1>
-        <h1>Home</h1>
-        <h1>Home</h1>
-        <h1>Home</h1>
-        <h1>Home</h1>
-        <h1>Home</h1>
-        <h1>Home</h1>
-        <h1>Home</h1>
-      </div>
-    </>
+    <div
+      style={{
+        backgroundImage: 'url("/bg-img.jpg")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Container>
+        <Card style={{ backgroundColor: "rgba(255, 255, 255, 0.8)" }}>
+          <CardContent>
+            <Typography variant="h2" gutterBottom>
+              Currency Dashboard
+            </Typography>
+            <Button
+              component={Link}
+              to="/currency-converter"
+              variant="contained"
+              color="primary"
+              style={{ marginRight: 10 }}
+            >
+              Currency Converter
+            </Button>
+            <Button
+              component={Link}
+              to="/historical"
+              variant="contained"
+              color="primary"
+              style={{ marginRight: 10 }}
+            >
+              Historical Data
+            </Button>
+            <Button
+              component={Link}
+              to="/forex-news"
+              variant="contained"
+              color="primary"
+            >
+              Forex News
+            </Button>
+          </CardContent>
+        </Card>
+      </Container>
+    </div>
   );
-}
+};
+
+export default Home;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,12 +7,12 @@ export const Layout = () => {
   return (
     <>
       <Navbar />
-      <Container max-width={true} sx={{ mt: 4, pb: 6, height: "70vh" }}>
+      {/* <Container max-width={false} sx={{ mt: 4, pb: 6, height: "70vh" }}> */}
         <Outlet />
-      </Container>
-      <Container maxWidth={false} disableGutters sx={{ mb: 0, width: "100%" }}>
-        <Footer />
-      </Container>
+      {/* </Container> */}
+      {/* <Container max-width={false} disableGutters sx={{ mb: 0, width: "100%" }}> */}
+        {/* <Footer /> */}
+      {/* </Container> */}
     </>
   );
 };

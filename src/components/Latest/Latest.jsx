d5683b1f104afc6e569884be04bfbcd32904356e
Marcel George Ionite <george_ionite@yahoo.com>
2024-02-09 19:31:21 +0000

    currency conversion added.

--- src/components/Latest/Latest.jsx
+++ src/components/Latest/Latest.jsx
@@ -1,18 +1,16 @@
-<<<<<<< HEAD
 import './Latest.css'
 import React, { useState, useEffect } from 'react';
 
-const CurrencyConverter = () => {
+const Latest = () => {
   const [amount, setAmount] = useState(1);
   const [fromCurrency, setFromCurrency] = useState('EUR');
   const [toCurrency, setToCurrency] = useState('USD');
   const [exchangeRate, setExchangeRate] = useState(null);
 
-  const apiKey = 'fca_live_tL8hRYWmucQJ0vkodjxyS90TOBweprVAZb51VwFB'; // Replace with your Freecurrencyapi.com API key
-
+  const apiKey = 'fca_live_tL8hRYWmucQJ0vkodjxyS90TOBweprVAZb51VwFB'; 
   useEffect(() => {
     if (fromCurrency != null && toCurrency != null) {
-      fetch(`https://freecurrencyapi.com/api/v2/latest?apikey=${apiKey}&base_currency=${fromCurrency}`)
+      fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}&base_currency=${fromCurrency}`)
         .then((res) => res.json())
         .then((data) => {
           setExchangeRate(data.data[toCurrency]);
@@ -40,42 +38,79 @@ const CurrencyConverter = () => {
         <select value={fromCurrency} onChange={handleFromCurrencyChange}>
           <option value="EUR">EUR</option>
           <option value="USD">USD</option>
-          {/* Add more currency options here */}
+          <option value="JPY">JPY</option>
+          <option value="BGN">BGN</option>
+          <option value="CZK">CZK</option>
+          <option value="DKK">DKK</option>
+          <option value="GBP">GBP</option>
+          <option value="HUF">HUF</option>
+          <option value="PLN">PLN</option>
+          <option value="RON">RON</option>
+          <option value="SEK">SEK</option>
+          <option value="CHF">CHF</option>
+          <option value="ISK">ISK</option>
+          <option value="NOK">NOK</option>
+          <option value="HRK">HRK</option>
+          <option value="RUB">RUB</option>
+          <option value="TRY">TRY</option>
+          <option value="AUD">AUD</option>
+          <option value="BRL">BRL</option>
+          <option value="CAD">CAD</option>
+          <option value="CNY">CNY</option>
+          <option value="HKD">HKD</option>
+          <option value="IDR">IDR</option>
+          <option value="ILS">ILS</option>
+          <option value="INR">INR</option>
+          <option value="KRW">KRW</option>
+          <option value="MXN">MXN</option>
+          <option value="MYR">MYR</option>
+          <option value="NZD">NZD</option>
+          <option value="PHP">PHP</option>
+          <option value="SGD">SGD</option>
+          <option value="THB">THB</option>
+          <option value="ZAR">ZAR</option>
         </select>
         <span>=</span>
         <input type="text" value={amount * (exchangeRate || 0)} disabled />
         <select value={toCurrency} onChange={handleToCurrencyChange}>
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
-          {/* Add more currency options here */}
+          <option value="JPY">JPY</option>
+          <option value="BGN">BGN</option>
+          <option value="CZK">CZK</option>
+          <option value="DKK">DKK</option>
+          <option value="GBP">GBP</option>
+          <option value="HUF">HUF</option>
+          <option value="PLN">PLN</option>
+          <option value="RON">RON</option>
+          <option value="SEK">SEK</option>
+          <option value="CHF">CHF</option>
+          <option value="ISK">ISK</option>
+          <option value="NOK">NOK</option>
+          <option value="HRK">HRK</option>
+          <option value="RUB">RUB</option>
+          <option value="TRY">TRY</option>
+          <option value="AUD">AUD</option>
+          <option value="BRL">BRL</option>
+          <option value="CAD">CAD</option>
+          <option value="CNY">CNY</option>
+          <option value="HKD">HKD</option>
+          <option value="IDR">IDR</option>
+          <option value="ILS">ILS</option>
+          <option value="INR">INR</option>
+          <option value="KRW">KRW</option>
+          <option value="MXN">MXN</option>
+          <option value="MYR">MYR</option>
+          <option value="NZD">NZD</option>
+          <option value="PHP">PHP</option>
+          <option value="SGD">SGD</option>
+          <option value="THB">THB</option>
+          <option value="ZAR">ZAR</option>
         </select>
       </div>
     </div>
   );
 };
 
-export default CurrencyConverter;
-=======
-export default function Latest() {
-  const apiKey = "fca_live_tL8hRYWmucQJ0vkodjxyS90TOBweprVAZb51VwFB";
-  const url = `https://freecurrencyapi.com/api/v2/latest?apikey=${apiKey}&base_currency=USD`;
+export default Latest;
 
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (data.code === 200) {
-        console.log("Exchange rates:", data.data);
-      } else {
-        console.error("Error fetching data:", data.message);
-      }
-    })
-    .catch((e) => {
-      console.error("Error: ", e);
-    });
-}
->>>>>>> a772bb7094273865d506fcafc66b47c6222da68d
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-<<<<<<< HEAD
 import { registerLicense } from '@syncfusion/ej2-base';
-
-registerLicense("ORg4AjUWIQA/Gnt2UVhiQlVPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9nSXxTfkRnW31ed3RcQGQ=");
-=======
-import { registerLicense } from "@syncfusion/ej2-base";
->>>>>>> a772bb7094273865d506fcafc66b47c6222da68d
 
 registerLicense(import.meta.env.VITE_SYNCFUSION_API);
 ReactDOM.createRoot(document.getElementById("root")).render(

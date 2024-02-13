7dd8428763ca8f2378ead80ac18342bd7af886d0
Noah Sheldon <noahsheldon06@gmail.com>
2024-02-13 20:31:42 +0000

    forex news component styled

--- src/components/ForexNews/ForexNews.jsx
+++ src/components/ForexNews/ForexNews.jsx
@@ -1,142 +1,129 @@
-import React, { useState, useEffect } from "react";
-import currencies from "../../data/currencies"; // Import currency options
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import React, { useState, useEffect } from "react";
+import currencies from "../../data/currencies";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Button,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Grid,
+  TextField,
+} from "@mui/material";
+import AdapterDateFns from "@mui/material/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DatePicker from "@mui/lab/DatePicker";
 
 const ForexNews = () => {
-  const apiKey = "80db4a5790bef82dcaa9bc8d7de2cd7c"; // GNews API key (please modify this ,put it in the .env - I couldnt locate it
-  const [news, setNews] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [filteredNews, setFilteredNews] = useState([]);
-  const [selectedCurrency, setSelectedCurrency] = useState("USD");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [selectedLanguage, setSelectedLanguage] = useState("en");
+  const [selectedCurrency, setSelectedCurrency] = useState("");
+  const [selectedLanguage, setSelectedLanguage] = useState("");
+  const [fromDate, setFromDate] = useState(null);
+  const [toDate, setToDate] = useState(null);
+  const [newsData, setNewsData] = useState([]);
 
-  useEffect(() => {
-    const fetchForexNews = async () => {
-      try {
-        let apiUrl = `https://gnews.io/api/v4/search?q=forex+${selectedCurrency}&token=${apiKey}`;
-        if (startDate && endDate) {
-          apiUrl += `&from=${startDate}&to=${endDate}`;
-        }
-        apiUrl += `&lang=${selectedLanguage}`;
+  const handleShowNews = async () => {
+    const apiKey = import.meta.env.VITE_GNEWS_API_KEY;
+    const apiUrl = `https://gnews.io/api/v4/search?q=forex+${selectedCurrency}&token=${apiKey}&lang=${selectedLanguage}&from=${fromDate}&to=${toDate}`;
 
-        const response = await fetch(apiUrl);
-        if (response.ok) {
-          const data = await response.json();
-          setNews(data.articles);
-        } else {
-          console.error("Failed to fetch Forex news");
-        }
-      } catch (error) {
-        console.error("Error fetching Forex news:", error);
-      }
-    };
-
-    fetchForexNews();
-  }, [selectedCurrency, startDate, endDate, selectedLanguage]);
-
-  useEffect(() => {
-    const filteredArticles = news.filter((article) =>
-      article.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredNews(filteredArticles);
-  }, [searchTerm, news]);
-
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleCurrencyChange = (event) => {
-    setSelectedCurrency(event.target.value);
-  };
-
-  const handleStartDateChange = (event) => {
-    setStartDate(event.target.value);
-  };
-
-  const handleEndDateChange = (event) => {
-    setEndDate(event.target.value);
-  };
-
-  const handleLanguageChange = (event) => {
-    setSelectedLanguage(event.target.value);
+    try {
+      // Fetch data from the API
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      setNewsData(data.articles || []);
+    } catch (error) {
+      console.error("Error fetching news data:", error);
+    }
   };
 
   return (
-    <div className="container">
-      <h2 className="my-4">Forex News</h2>
-      <input
-        type="text"
-        className="form-control mb-3"
-        placeholder="Search news..."
-        value={searchTerm}
-        onChange={handleSearchChange}
-      />
-      <div className="row mb-3">
-        <div className="col-md-4">
-          <select
-            className="form-select"
-            value={selectedCurrency}
-            onChange={handleCurrencyChange}
-          >
-            {currencies.map((currency) => (
-              <option key={currency.value} value={currency.label}>
-                {currency.value} -- {currency.label}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="col-md-4">
-          <input
-            type="date"
-            className="form-control"
-            placeholder="Start Date"
-            value={startDate}
-            onChange={handleStartDateChange}
-          />
-        </div>
-        <div className="col-md-4">
-          <input
-            type="date"
-            className="form-control"
-            placeholder="End Date"
-            value={endDate}
-            onChange={handleEndDateChange}
-          />
-        </div>
-        <div className="col-md-4">
-          <select
-            className="form-select"
-            value={selectedLanguage}
-            onChange={handleLanguageChange}
-          >
-            <option value="en">English</option>
-            <option value="fr">French</option>
-            <option value="de">German</option>
-            {/* Add more language options as needed */}
-          </select>
-        </div>
-      </div>
-      <div className="row row-cols-1 row-cols-md-2 g-4">
-        {filteredNews.map((article, index) => (
-          <div key={index} className="col">
-            <div className="card h-100">
-              <div className="card-body">
-                <h5 className="card-title">{article.title}</h5>
-                <h6 className="card-title">{article.publishedAt.slice(0, 10)}</h6>
-                <p className="card-text">{article.description}</p>
-                <a
+    <div className="container mt-3">
+      <FormControl>
+        <Select
+          value={selectedCurrency}
+          onChange={(e) => setSelectedCurrency(e.target.value)}
+          displayEmpty
+          sx={{ marginRight: "20px", marginBottom: "30px" }}
+        >
+          <MenuItem value="" disabled>
+            Select Currency
+          </MenuItem>
+          {currencies.map((currency) => (
+            <MenuItem key={currency.label} value={currency.label}>
+              {currency.value}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+
+      <FormControl>
+        <Select
+          value={selectedLanguage}
+          onChange={(e) => setSelectedLanguage(e.target.value)}
+          displayEmpty
+          sx={{ marginRight: "20px", marginBottom: "30px" }}
+        >
+          <MenuItem value="" disabled>
+            Select Language
+          </MenuItem>
+          <MenuItem value="fr">French</MenuItem>
+          <MenuItem value="en">English</MenuItem>
+          <MenuItem value="de">German</MenuItem>
+        </Select>
+      </FormControl>
+
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DatePicker
+              label="From Date"
+              value={fromDate}
+              onChange={(newValue) => setFromDate(newValue)}
+              renderInput={(params) => <TextField {...params} fullWidth />}
+            />
+          </LocalizationProvider>
+        </Grid>
+
+        <Grid item xs={6}>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DatePicker
+              label="To Date"
+              value={toDate}
+              onChange={(newValue) => setToDate(newValue)}
+              renderInput={(params) => <TextField {...params} fullWidth />}
+            />
+          </LocalizationProvider>
+        </Grid>
+      </Grid>
+
+      <Button variant="contained" color="primary" onClick={handleShowNews}>
+        Show News
+      </Button>
+
+      <div className="row mt-3">
+        {newsData.map((article, index) => (
+          <div key={index} className="col-md-4">
+            <Card>
+              <img src={article.image} className="card-img-top" alt="News" />
+              <CardContent>
+                <Typography variant="h6">{article.title}</Typography>
+                <Typography variant="body2">{article.description}</Typography>
+              </CardContent>
+              <CardActions>
+                <Button
                   href={article.url}
-                  className="btn btn-primary"
                   target="_blank"
                   rel="noopener noreferrer"
+                  variant="primary"
+                  sx={{ backgroundColor: "#4591A7", color: "white" }}
                 >
                   Read More
-                </a>
-              </div>
-            </div>
+                </Button>
+              </CardActions>
+            </Card>
           </div>
         ))}
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+<<<<<<< HEAD
 import { registerLicense } from "@syncfusion/ej2-base";
 
 registerLicense(import.meta.env.VITE_SYNCFUSION_API);
+=======
+
+>>>>>>> 372a709 (forex news component styled)
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />

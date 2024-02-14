41753137135e1c41ca125520623510ed3217fd93
Noah Sheldon <noahsheldon06@gmail.com>
2024-02-14 18:42:45 +0000

    navbar icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -114,7 +114,7 @@ function Navbar() {
               </MenuItem>
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <EjectIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
